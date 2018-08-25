e320032d95bf82a8586b81f36d931061d1ce40ae
EC2 Default User <ec2-user@ip-172-31-14-135.us-east-2.compute.internal>
2018-08-25 03:17:49 +0000

    no.1

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,25 @@
+class ApplicationController < ActionController::Base
+   before_action :set_current_user
+   
+    def set_current_user
+        @current_user = User.find_by(id: session[:user_id])
+    end
+    def authenticate_user
+      if session[:user_id] == nil
+        flash[:notice] = "ログインしてください"
+        redirect_to("/login")
+      end
+    end
+    def forbit_login_user
+        if session[:user_id]
+            flash[:notice]="すでにログインしています"
+            redirect_to("/posts/index")
+        end
+    end
+    def ensure_correct_user
+      if @current_user.id != params[:id].to_i
+        flash[:notice] = "権限がありません"
+        redirect_to("/users/#{@current_user.id}")
+      end
+    end
+end
